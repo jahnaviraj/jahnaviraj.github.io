65e9fc60c36e516a7f85910f2d2c83ce67905b4e
jahnaviraj <jahnaviraj25@gmail.com>
2022-03-07 20:45:00 +0530

    add main.py

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
-###FLASK APP FOR PORTFOLIO WEBSITE###
+###FLASK APP FOR PORTFOLIO WEBSITE##
+ 
+from re import sub
+from flask import Flask, render_template, request, flash
+import smtplib, os
+from dotenv import load_dotenv
 
-import flask
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('secret_key')
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        admin_email = os.getenv('contact_email')
+        admin_password = os.getenv('contact_password')
+        print(name,email,subject,message,admin_email,admin_password)
+        with smtplib.SMTP('smtp.gmail.com', 587) as conn:
+            conn.ehlo()
+            conn.starttls()
+            conn.login(admin_email, admin_password)
+            conn.sendmail(admin_email , "jahnaviraj25@gmail.com", f'Subject: {subject} \n\n{message}\n\n-{name}\n{email}')
+            conn.quit()
+
+            flash("Your message was successfully sent. Thankyou!")
+
+    return render_template('index.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
