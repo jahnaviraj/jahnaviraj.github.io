e67e6d1e546d492c062245cc9a322c45c3bab2d1
jahnaviraj <jahnaviraj25@gmail.com>
2022-03-07 21:17:18 +0530

    undo smtplib

--- main.py
+++ main.py
@@ -12,20 +12,6 @@ app.config['SECRET_KEY'] = os.getenv('secret_key')
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        subject = request.form["subject"]
-        message = request.form["message"]
-        # with smtplib.SMTP('smtp.gmail.com', 587) as conn:
-        #     conn.ehlo()
-        #     conn.starttls()
-        #     conn.login(os.getenv('contact_email'), os.getenv('contact_password'))
-        #     conn.sendmail(os.getenv('contact_email') , os.getenv('send_to_email'), f'Subject: {subject} \n\n{message}\n\n-{name}\n{email}')
-        #     conn.quit()
-
-        #     flash("Your message was successfully sent. Thankyou!")
-
     return render_template('index.html')
 
 
